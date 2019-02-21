4f9c421ebc846a50816f6070e4b1a9d3a54a5029
Nick Harding <njh@well.ox.ac.uk>
2019-02-21 14:27:43 +0000

    remove logging

--- pipelines/qc/submit.sh
+++ pipelines/qc/submit.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
-
-mkdir -p _log
 snakemake --snakefile `dirname $0`/Snakefile $@ \
-    --cluster 'qsub -v PATH="/home/njh/miniconda3/bin:$PATH" -j y -o $(pwd)/_log/ -b n -l {params.req},h=!foxtrot.well.ox.ac.uk -S /bin/bash' \
+    --cluster 'qsub -v PATH="/home/njh/miniconda3/bin:$PATH" -j y -b n -l {params.req},h=!foxtrot.well.ox.ac.uk -S /bin/bash' \
     --jobs 50 \
     --latency-wait 300 \
     --restart-times 0 \
