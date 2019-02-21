e5b00aafc9b90fe9f60c56065689808defc8477a
Nick Harding <njh@well.ox.ac.uk>
2019-02-21 15:13:46 +0000

    Fix conda path

--- pipelines/qc/submit.sh
+++ pipelines/qc/submit.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 snakemake --snakefile `dirname $0`/Snakefile $@ \
-    --cluster 'qsub -v PATH="/home/njh/miniconda3/bin:$PATH" -j y -b n -l {params.req},h=!foxtrot.well.ox.ac.uk -S /bin/bash' \
+    --cluster 'qsub -v PATH="binder/deps/conda/bin::$PATH" -j y -b n -l {params.req},h=!foxtrot.well.ox.ac.uk -S /bin/bash' \
     --jobs 50 \
     --latency-wait 300 \
     --restart-times 0 \
