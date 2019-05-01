ba2019327d0ba7d4a50ac37fd05a03da2df65531
Nick Harding <njh@well.ox.ac.uk>
2019-05-01 15:27:35 +0100

    Change submit script
    lower load on sge
    avoids possible race condition
    auto restart

--- pipelines/combine-zarr-sample-genotypes/sge-submit.sh
+++ pipelines/combine-zarr-sample-genotypes/sge-submit.sh
@@ -2,7 +2,10 @@
 snakemake --snakefile `dirname $0`/Snakefile $@ \
     --cluster 'qsub -v PATH="binder/deps/conda/bin::$PATH" -j y -b n -l {params.req},h=!foxtrot.well.ox.ac.uk -S /bin/bash' \
     --jobs 50 \
+    --ri \
     --latency-wait 300 \
-    --restart-times 0 \
+    --max-jobs-per-second 0.1 \
+    --max-status-checks-per-second 0.1 \
+    --restart-times 1 \
     --jn "{rulename}.{jobid}.sh" \
     --use-conda
