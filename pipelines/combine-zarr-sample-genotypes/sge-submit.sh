998d9c5e056a6456ef1288905ba11e0124f53d78
Nick Harding <njh@well.ox.ac.uk>
2019-05-30 14:57:38 +0100

    remove conda env

--- pipelines/combine-zarr-sample-genotypes/sge-submit.sh
+++ pipelines/combine-zarr-sample-genotypes/sge-submit.sh
@@ -2,10 +2,7 @@
 snakemake --snakefile `dirname $0`/Snakefile $@ \
     --cluster 'qsub -v PATH="binder/deps/conda/bin::$PATH" -j y -b n -l {params.req},h=!foxtrot.well.ox.ac.uk -S /bin/bash' \
     --jobs 50 \
-    --ri \
     --latency-wait 300 \
-    --max-jobs-per-second 0.1 \
-    --max-status-checks-per-second 0.1 \
-    --restart-times 1 \
+    --restart-times 0 \
     --jn "{rulename}.{jobid}.sh" \
     --use-conda
