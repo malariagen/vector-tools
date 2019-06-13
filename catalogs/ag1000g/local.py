e1c9c170dba80c8d19ba44839985341135905fa0
Alistair Miles <alimanfoo@googlemail.com>
2019-06-13 12:06:10 +0100

    rework catalog setup

---
+++ catalogs/ag1000g/local.py
@@ -0,0 +1,52 @@
+import zarr
+from pathlib import Path
+from cached_property import cached_property
+
+
+# default base directories (can be overridden)
+AG1000G_RELEASE_DIR = Path("/kwiat/vector/ag1000g/release")
+PHASE1_AR3_DIR = AG1000G_RELEASE_DIR / 'phase1.AR3'
+PHASE2_AR1_DIR = AG1000G_RELEASE_DIR / 'phase2.AR1'
+
+
+class Phase1AR3(object):
+
+    def __init__(self, base_dir=PHASE1_AR3_DIR):
+        self.base_dir = Path(base_dir)
+
+    @cached_property
+    def variation_main(self):
+        path = self.base_dir / 'variation/main/zarr/ag1000g.phase1.ar3'
+        return zarr.open_consolidated(str(path))
+
+    @cached_property
+    def variation_main_pass(self):
+        path = self.base_dir / 'variation/main/zarr/ag1000g.phase1.ar3.pass'
+        return zarr.open_consolidated(str(path))
+
+    @cached_property
+    def variation_main_pass_biallelic(self):
+        path = self.base_dir / 'variation/main/zarr/ag1000g.phase1.ar3.pass.biallelic'
+        return zarr.open_consolidated(str(path))
+
+    
+class Phase2AR1(object):
+
+    def __init__(self, base_dir=PHASE2_AR1_DIR):
+        self.base_dir = Path(base_dir)
+
+    @cached_property
+    def variation_main(self):
+        path = self.base_dir / 'variation/main/zarr/all/ag1000g.phase2.ar1'
+        return zarr.open_consolidated(str(path))
+
+    @cached_property
+    def variation_main_pass():
+        path = self.base_dir / 'variation/main/zarr/pass/ag1000g.phase2.ar1.pass'
+        return zarr.open_consolidated(str(path))
+
+    @cached_property
+    def variation_main_pass_biallelic():
+        path = self.base_dir / 'variation/main/zarr/biallelic/ag1000g.phase2.ar1.pass.biallelic'
+        return zarr.open_consolidated(str(path))
+
