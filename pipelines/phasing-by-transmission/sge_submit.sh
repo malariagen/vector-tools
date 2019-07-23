ba15f4ba364851d95fce5521477eace74a4c2d8a
無 <4256466+leehart@users.noreply.github.com>
2019-07-23 16:51:58 +0100

    Make pipeline Snakemake params the same

--- pipelines/phasing-by-transmission/sge_submit.sh
+++ pipelines/phasing-by-transmission/sge_submit.sh
@@ -1,18 +1,7 @@
-set -e
-
-if [[ -z "${SGE_LOG}" ]]; then
-  SGE_LOG=$HOME
-  echo "SGE_LOG not set. The log location defaults to $SGE_LOG. Remember this variable needs to be exported."
-else
-  echo "SGE_LOG set as $SGE_LOG"
-fi
-
-mkdir -p $SGE_LOG
-
 #! /bin/bash
 snakemake --snakefile `dirname $0`/Snakefile $@ \
-    --cluster 'qsub -v PATH="binder/deps/conda/bin::$PATH" -j y -o $SGE_LOG -b n -l {params.req} -S /bin/bash' \
-    --jobs 10 \
+    --cluster 'qsub -v PATH="binder/deps/conda/bin::$PATH" -j y -b n -l {params.req} -S /bin/bash -o $(pwd)' \
+    --jobs 50 \
     --latency-wait 60 \
     --restart-times 0 \
     --jn "{rulename}.{jobid}.sh" \
--- pipelines/qc/submit.sh
+++ pipelines/qc/submit.sh
@@ -2,7 +2,7 @@
 snakemake --snakefile `dirname $0`/Snakefile $@ \
     --cluster 'qsub -v PATH="binder/deps/conda/bin::$PATH" -j y -b n -l {params.req} -S /bin/bash -o $(pwd)' \
     --jobs 50 \
-    --latency-wait 300 \
+    --latency-wait 60 \
     --restart-times 0 \
     --jn "{rulename}.{jobid}.sh" \
     --use-conda
