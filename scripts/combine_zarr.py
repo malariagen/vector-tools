a0e6e8dde11817f7d9d6779ac391f37b6c9f70b0
Nick Harding <njh@well.ox.ac.uk>
2019-07-24 14:36:55 +0100

    missing )

--- scripts/combine_zarr.py
+++ scripts/combine_zarr.py
@@ -115,7 +115,7 @@ def check_array_setup(samples, input_pattern, seqid, field):
     except KeyError:
         field = field.replace("calldata/", "variants/")
         array = callset[samples[0]][seqid][field]
-        log("{field} found in `variants` not `calldata`".format(field=field)
+        log("{field} found in `variants` not `calldata`".format(field=field))
 
     n_variants = array.shape[0]
     log('Found {:,} variants.'.format(n_variants))
